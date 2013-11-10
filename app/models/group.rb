85ddee861ab79e7805007f553159e847625b899c
Ray <huihong521@gmail.com>
2013-11-10 16:25:36 +0800

    Add four getters for each group and a default group getter.

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,24 @@ class Group < ActiveRecord::Base
 	has_many :users, through: :memberships, dependent: :destroy
 
 	validates :title, presence: true, uniqueness: true
+
+	def self.admin
+		Group.find_by_title('Admin')
+	end
+
+	def self.teacher
+		Group.find_by_title('Teacher')
+	end
+
+	def self.student
+		Group.find_by_title('Student')
+	end
+
+	def self.visitor
+		Group.find_by_title('Visitor')
+	end
+
+	def self.default_group
+		Group.visitor
+	end
 end
