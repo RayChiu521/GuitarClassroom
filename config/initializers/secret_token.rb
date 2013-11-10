22f8fda6a692a150856bc0b165240b43631d6491
Ray <huihong521@gmail.com>
2013-11-10 13:52:16 +0800

    Guitar classroom.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuitarClassroom::Application.config.secret_key_base = '2d3033f72595aa7226d882359a1b7d972c5e93e046e5f96f80394695bf9047b01ae7ab2ea946784fcfae6ec40d9419713dcb0c875f134429b5fd649ccf1cd698'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GuitarClassroom::Application.config.session_store :cookie_store, key: '_GuitarClassroom_session'
