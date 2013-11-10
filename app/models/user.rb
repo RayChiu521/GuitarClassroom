06bff244e02f31f88915f258f616c63c687999e4
Ray <huihong521@gmail.com>
2013-11-10 15:03:59 +0800

    Add the user authentication feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,31 @@
 # encoding: utf-8
 class User < ActiveRecord::Base
 
+	attr_accessor :password, :password_confirmation
+
 	has_many :memberships
 	has_many :groups, through: :memberships, dependent: :destroy
 	has_one :user_password, dependent: :destroy
 
 	validates :account, presence: true, uniqueness: true
+	validates :password, confirmation: true, presence: { on: :create }
+	validates :password_confirmation, presence: { on: :create }
 
+	before_save :encrypt_password
+
+	def encrypt_password
+    if password.present?
+    	self.user_password = UserPassword.new
+    	self.user_password.generate_password(password)
+    end
+  end
+
+  def self.authenticate(account, password)
+    user = find_by_account(account)
+    if user && user.user_password && user.user_password.authenticate_password(password)
+      user
+    else
+      nil
+    end
+  end
 end
--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -3,4 +3,14 @@ class UserPassword < ActiveRecord::Base
 
 	belongs_to :user
 
+	def generate_password(password)
+    if password.present?
+    	self.password_salt = BCrypt::Engine.generate_salt
+    	self.password_hash = BCrypt::Engine.hash_secret(password, self.password_salt)
+    end
+  end
+
+  def authenticate_password(password)
+  	self.password_hash == BCrypt::Engine.hash_secret(password, self.password_salt)
+  end
 end
