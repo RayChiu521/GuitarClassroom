7138965fb0cfe9d5eeeb450da7645e56935f48aa
Ray <huihong521@gmail.com>
2013-11-10 19:05:49 +0800

    Add bootstrap style control-group block.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def text_control_group(obj, field)
+		content_tag('div', class: 'control-group') do
+			concat obj.label field, class: 'control-label'
+			concat content_tag('div', obj.text_field(field), class: 'controls')
+		end
+	end
+
+	def password_control_group(obj, field)
+		content_tag('div', class: 'control-group') do
+			concat obj.label field, class: 'control-label'
+			concat content_tag('div', obj.password_field(field), class: 'controls')
+		end
+	end
+
 end
