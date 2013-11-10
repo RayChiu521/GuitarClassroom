bfe0ead5fd5363dcc426a8336b88ca070ca845ef
Ray <huihong521@gmail.com>
2013-11-10 14:48:14 +0800

    Add a model -- user_password that belongs to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
 	has_many :memberships
 	has_many :groups, through: :memberships, dependent: :destroy
+	has_one :user_password, dependent: :destroy
 
 	validates :account, presence: true, uniqueness: true
+
 end
---
+++ app/models/user_password.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+class UserPassword < ActiveRecord::Base
+
+	belongs_to :user
+
+end
