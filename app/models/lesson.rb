c639aa1cec6631367eab52252aa9f8b4be918c28
Ray <huihong521@gmail.com>
2013-11-23 22:46:06 +0800

    1. changed a relation name from arragements to arrangements. 2. added a virtual column current_user for validating. 3. added some validations. 4. overriden to_json method of lesson model for including teacher and students automatically.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,40 @@
 # encoding: utf-8
 class Lesson < ActiveRecord::Base
 
-	has_many :arragements
-	has_many :students, through: :arragements, source: :user, dependent: :destroy
+	attr_accessor :current_user
+
+	has_many :arrangements
+	has_many :students, through: :arrangements, source: :user, dependent: :destroy
 
 	belongs_to :teacher, class_name: 'User', foreign_key: 'user_id'
+
+	# validates :lesson_date, presence: true
+	validates :teacher, presence: true
+	validates :student_ids, presence: true
+	validates :current_user, presence: true
+	validate :validate_teacher_group, :validate_arrangement_for_others
+
+
+	def to_json(options={})
+		range = {
+			include: [
+				{ teacher: { only: [:id, :account, :nickname] }},
+				{ students: { only: [:id, :account, :nickname] }}
+			]
+		}
+  	super(options.merge(range))
+	end
+
+	private
+
+		def validate_teacher_group
+			errors.add(:teacher, "#{self.teacher.account} isn't a teacher.") unless self.teacher.is_teacher?
+	  end
+
+	  def validate_arrangement_for_others
+	  	if !self.current_user.id.in?(self.student_ids) && !self.current_user.is_teacher?
+				errors.add(:students, "You can't arrangement an lesson for other students.")
+			end
+	  end
+
 end
