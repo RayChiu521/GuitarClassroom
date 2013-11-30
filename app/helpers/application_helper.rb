d05200347e2c846630475a8dacd79ee0a9f0f798
Ray <huihong521@gmail.com>
2013-11-30 15:01:25 +0800

    Added two helpers -- display_datetime and textarea_control_group.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+	def display_datetime(dt)
+		!dt.nil? ? dt.strftime("%Y-%m-%d %l:%M") : ''
+	end
+
 	def text_control_group(obj, field)
 		content_tag('div', class: 'form-group') do
 			concat obj.label field, class: 'col-md-3 control-label'
@@ -10,6 +14,13 @@ module ApplicationHelper
 		content_tag('div', class: 'form-group') do
 			concat obj.label field, class: 'col-md-3 control-label'
 			concat content_tag('div', obj.password_field(field, class: 'form-control'), class: 'col-md-9')
+		end
+	end
+
+	def textarea_control_group(obj, field)
+		content_tag('div', class: 'form-group') do
+			concat obj.label field, class: 'col-md-3 control-label'
+			concat content_tag('div', obj.text_area(field, class: 'form-control'), class: 'col-md-9')
 		end
 	end
 
