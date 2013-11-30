c51f673cfa7e3a7d308baf29a934cb4d4b20cf71
Ray <huihong521@gmail.com>
2013-11-30 18:12:04 +0800

    Added a logged_in? helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
 
   before_action :require_login
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !!current_user
+  end
 
   private
   def current_user
