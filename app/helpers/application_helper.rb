a6027a1ccfc8849a3df34aa78adcf381696ea538
Ray <huihong521@gmail.com>
2013-12-01 12:17:07 +0800

    1. Renamed argument html to html_attributes. 2. Added the method default_html_attributes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,10 @@ module ApplicationHelper
 		!dt.nil? ? dt.strftime("%Y-%m-%d %l:%M") : ''
 	end
 
-	def text_control_group(obj, field, html = {})
-		if !html.has_key?(:class)
-			html[:class] = 'form-control'
-		else
-			html[:class] += ' form-control'
-		end
+	def text_control_group(obj, field, html_attributes = {})
 		content_tag(:div, class: 'form-group') do
 			concat obj.label field, class: 'col-md-3 control-label'
-			concat content_tag(:div, obj.text_field(field, html), class: 'col-md-9')
+			concat content_tag(:div, obj.text_field(field, default_html_attributes(html_attributes)), class: 'col-md-9')
 		end
 	end
 
@@ -29,11 +24,21 @@ module ApplicationHelper
 		end
 	end
 
-	def textarea_control_group(obj, field)
+	def textarea_control_group(obj, field, html_attributes = {})
 		content_tag(:div, class: 'form-group') do
 			concat obj.label field, class: 'col-md-3 control-label'
-			concat content_tag(:div, obj.text_area(field, class: 'form-control'), class: 'col-md-9')
+			concat content_tag(:div, obj.text_area(field, default_html_attributes(html_attributes)), class: 'col-md-9')
 		end
+	end
+
+	def default_html_attributes(html_attributes)
+		if !html_attributes.has_key?(:class)
+			html_attributes[:class] = 'form-control'
+		else
+			html_attributes[:class] += ' form-control'
+		end
+
+		html_attributes
 	end
 
 
